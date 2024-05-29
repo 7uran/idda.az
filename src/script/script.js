729f77883703bdb6e6d1c7b1d0b990ebba4497e4
7uran <turan.official21@gmail.com>
2024-05-30 01:11:26 +0400

    Finished

---
+++ src/script/script.js
@@ -0,0 +1,7 @@
+const searchbtn = document.querySelector(".search-btn")
+const input = document.querySelector(".right-th input")
+
+searchbtn.addEventListener("click", () => {
+    input.style = "visibility:visible;"
+
+})
